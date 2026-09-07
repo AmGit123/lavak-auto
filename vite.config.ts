c0010f8119734ee8bf2459c35c96ed52da7d38cb
BAH Amadou <amlaabha123@gmail.com>
2026-09-07 07:28:06 +0200

    Configure Cloudflare D1 database

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import hostingConfig from "./.openai/hosting.json";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "d4fb195f-4542-417a-a064-317bdde76017";
 
 const { d1, r2 } = hostingConfig;
 
